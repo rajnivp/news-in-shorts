f38b4f02810e66ddfa28d92e815018a34ec14142
Rajni-Padhariya <rajnipadhariya424@gmail.com>
2020-06-25 19:57:11 +0530

    fix

--- static/js/main.js
+++ static/js/main.js
@@ -256,46 +256,11 @@
 
 	};
 
-	var stickyFunction = function() {
-
-		var h = $('.image-content').outerHeight();
-
-		if ($(window).width() <= 992 ) {
-			$("#sticky_item").trigger("sticky_kit:detach");
-		} else {
-			$('.sticky-parent').removeClass('stick-detach');
-			$("#sticky_item").trigger("sticky_kit:detach");
-			$("#sticky_item").trigger("sticky_kit:unstick");
-		}
-
-		$(window).resize(function(){
-			var h = $('.image-content').outerHeight();
-			$('.sticky-parent').css('height', h);
-
-
-			if ($(window).width() <= 992 ) {
-				$("#sticky_item").trigger("sticky_kit:detach");
-			} else {
-				$('.sticky-parent').removeClass('stick-detach');
-				$("#sticky_item").trigger("sticky_kit:detach");
-				$("#sticky_item").trigger("sticky_kit:unstick");
-
-				$("#sticky_item").stick_in_parent();
-			}
-		});
-
-		$('.sticky-parent').css('height', h);
-
-		$("#sticky_item").stick_in_parent();
-
-	};
-
 	// Document on load.
 	$(function(){
 	    activeClass();
 		fullHeight();
 		burgerMenu();
 		mobileMenuOutsideClick();
-		stickyFunction();
 	});
 }());
